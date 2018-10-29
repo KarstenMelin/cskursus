f613b3668508d32c6e4017a2336fe7205cc446e5
admin <admin@TA-828-08>
2018-10-29 15:52:17 +0100

    Karsten

---
+++ TalOpgave/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TalOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int heltal = 10;
+            heltal++;
+            heltal--;
+            heltal += 20;
+            Console.WriteLine($"Heltallet er: {heltal}");
+
+            Console.WriteLine();
+
+
+
+
+
+            double kommatal = 12.5;
+
+            kommatal++;
+
+            kommatal--;
+
+            kommatal *= 2;
+
+            Console.WriteLine($"Kommatal = {kommatal}");
+
+
+            // Hold console åben ved debug
+
+            if (System.Diagnostics.Debugger.IsAttached)
+
+            {
+
+                Console.Write("Press any key to continue . . . ");
+
+                Console.ReadKey();
+                                           
+            }
+        }
+    }
+}
+
---
+++ VariaelDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿/// <summary>
+/// 
+/// </summary>
+
+using System;
+
+namespace VariabelDemo
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int a = 12;
+            var b = "kurt";
+
+
+            for (int i = 0; i < a; i++)
+            {
+                string s = "Hej " + b;
+                Console.WriteLine(s);
+                //       Skriv(s); 
+            }
+            Console.ReadKey();
+        }
+        
+        public class Skriv(string k)
+            {
+            Console.Writeline(ConsoleKey);
+            }
+}
